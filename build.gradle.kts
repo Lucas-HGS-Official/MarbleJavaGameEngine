255dd423218fee485126e32b40aabece10f14a93
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-09-08 17:12:28 -0300

    Adding LWJGL to the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,3 +17,25 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+val lwjglVersion = "3.3.4"
+val jomlVersion = "1.10.7"
+val lwjglNatives = "natives-windows"
+
+dependencies {
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-assimp")
+    implementation("org.lwjgl", "lwjgl-glfw")
+    implementation("org.lwjgl", "lwjgl-openal")
+    implementation("org.lwjgl", "lwjgl-opengl")
+    implementation("org.lwjgl", "lwjgl-stb")
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-assimp", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
+    implementation("org.joml", "joml", jomlVersion)
+}
