f924afab2bb55d792235d1bbff5e727dc100d259
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-09-10 10:02:32 -0300

    Created a Mouse Listener Singleton

---
+++ src/main/java/Marble/MouseListener.java
@@ -0,0 +1,35 @@
+package Marble;
+
+public class MouseListener {
+    private static MouseListener instance;
+    private double scrollX, scrollY;
+    private double xPos, yPos, lastX, lastY;
+    private boolean mouseButtonPressed[] = new boolean[3];
+    private boolean isDragging;
+
+    private MouseListener() {
+        this.scrollX = 0.0;
+        this.scrollY = 0.0;
+        this.xPos = 0.0;
+        this.yPos = 0.0;
+        this.lastX = 0.0;
+        this.lastY = 0.0;
+    }
+
+    public static MouseListener get() {
+        if (MouseListener.instance == null) {
+            MouseListener.instance = new MouseListener();
+        }
+
+        return instance;
+    }
+
+    public static void mousePosCallback(long window, double xPos, double yPos) {
+        get().lastX = get().xPos;
+        get().lastY = get().yPos;
+        get().xPos = xPos;
+        get().yPos = yPos;
+    }
+
+    public static void mouseButtonCallback() {}
+}
