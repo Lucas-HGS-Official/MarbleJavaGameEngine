9cfad4ac5eabaaad8f148b35a4e37d5eb1d5b8c6
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-09-17 09:58:36 -0300

    Simple key press detection for testing the scene

--- src/main/java/Marble/LevelScene.java
+++ src/main/java/Marble/LevelScene.java
@@ -1,5 +1,7 @@
 package Marble;
 
+import java.awt.event.KeyEvent;
+
 public class LevelScene extends Scene {
     public LevelScene() {
         
@@ -7,6 +9,8 @@ public class LevelScene extends Scene {
 
     @Override
     public void update(float dt) {
-        
+        if (KeyListener.isKeyPressed(KeyEvent.VK_SPACE)) {
+            int test = 0;
+        }
     }
 }
