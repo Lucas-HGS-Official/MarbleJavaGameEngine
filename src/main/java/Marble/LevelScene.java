7e67579fdd5f1e14c73c26ace91d420b3f146bd0
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-09-17 12:29:23 -0300

    Creating the skeleton of a "fade to black" scene transition

--- src/main/java/Marble/LevelScene.java
+++ src/main/java/Marble/LevelScene.java
@@ -3,14 +3,24 @@ package Marble;
 import java.awt.event.KeyEvent;
 
 public class LevelScene extends Scene {
+
+    private boolean changingScene = false;
+    private float timeToChangeScene = 2.f;
+
     public LevelScene() {
         
     }
 
     @Override
     public void update(float dt) {
-        if (KeyListener.isKeyPressed(KeyEvent.VK_SPACE)) {
-            int test = 0;
+        if (!changingScene && KeyListener.isKeyPressed(KeyEvent.VK_SPACE)) {
+            changingScene = true;
+        }
+
+        if (changingScene && timeToChangeScene > 0) {
+            timeToChangeScene -= dt;
+        } else if (changingScene) {
+            Window.changeScene(1);
         }
     }
 }
