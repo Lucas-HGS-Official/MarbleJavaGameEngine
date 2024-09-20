b6cc90cf956990e8627fc96a36e5c214d57ba8f9
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-09-20 13:20:48 -0300

    Compiled the Vertex and Fragment shaders

--- src/main/java/Marble/LevelEditorScene.java
+++ src/main/java/Marble/LevelEditorScene.java
@@ -1,8 +1,70 @@
 package Marble;
 
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL20.glGetShaderi;
+
 public class LevelEditorScene extends Scene {
 
+    private String vertexShaderSrc = "#version 330 core\n" +
+            "\n" +
+            "layout (location=0) in vec3 aPos;\n" +
+            "layout (location=1) in vec4 aColor;\n" +
+            "\n" +
+            "out vec4 fColor;\n" +
+            "\n" +
+            "void main() {\n" +
+            "    fColor = aColor;\n" +
+            "    gl_Position = vec4(aPos, 1.0);\n" +
+            "}";
+
+    private String fragmentShaderSrc = "#version 330 core\n" +
+            "\n" +
+            "in vec4 fColor;\n" +
+            "\n" +
+            "out vec4 color;\n" +
+            "\n" +
+            "void main() {\n" +
+            "    color = fColor;\n" +
+            "}";
+
+    private int vertexID, fragmentID, shaderProgram;
+
     public LevelEditorScene() {
+    }
+
+    @Override
+    public void init() {
+        /////////////////////////////////////////////////
+        // Compile & link Shaders
+        /////////////////////////////////////////////////
+
+        // Vertex Shader
+        vertexID = glCreateShader(GL_VERTEX_SHADER);
+
+        glShaderSource(vertexID, vertexShaderSrc);
+        glCompileShader(vertexID);
+
+        int success = glGetShaderi(vertexID, GL_COMPILE_STATUS);
+        if (success == GL_FALSE) {
+            int len = glGetShaderi(vertexID, GL_INFO_LOG_LENGTH);
+            System.out.println("ERROR: default.dlsl\n\tVertex shader compilation failed.");
+            System.out.println(glGetShaderInfoLog(vertexID, len));
+            assert false : "";
+        }
+
+        // Fragment Shader
+        fragmentID = glCreateShader(GL_FRAGMENT_SHADER);
+
+        glShaderSource(fragmentID, fragmentShaderSrc);
+        glCompileShader(fragmentID);
+
+        success = glGetShaderi(fragmentID, GL_COMPILE_STATUS);
+        if (success == GL_FALSE) {
+            int len = glGetShaderi(fragmentID, GL_INFO_LOG_LENGTH);
+            System.out.println("ERROR: default.dlsl\n\tFragment shader compilation failed.");
+            System.out.println(glGetShaderInfoLog(fragmentID, len));
+            assert false : "";
+        }
     }
 
     @Override
